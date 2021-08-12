acfd09ed22d29fa58687a3889b9e2fafde7416a5
Ariel Santos <ariel.santos@dcc.ufmg.br>
2021-08-11 21:30:31 -0300

    Teste básico para Login

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import patch, call
 
 from django.test import TestCase
 
@@ -6,9 +6,7 @@ from django.test import TestCase
 class SendLoginEmailViewTest(TestCase):
 
     def test_redirects_to_home_page(self):
-        response = self.client.post('/accounts/send_login_email', data={
-            'email': 'edith@example.com'
-        })
+        response = self.client.get('/accounts/login?token=abcd123')
         self.assertRedirects(response, '/')
 
     @patch('accounts.views.send_mail')
@@ -22,3 +20,27 @@ class SendLoginEmailViewTest(TestCase):
         self.assertEqual(subject, 'Your login link for Superlists')
         self.assertEqual(from_email, 'noreply@superlists')
         self.assertEqual(to_list, ['edith@example.com'])
+
+    def test_adds_success_message(self):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        }, follow=True)
+
+        message = list(response.context['messages'])[0]
+        self.assertEqual(
+            message.message,
+            "Check your email, we've sent you a link you can use to log in."
+        )
+        self.assertEqual(message.tags, "success")
+
+    @patch('accounts.views.messages')
+    def test_adds_success_message_with_mocks(self, mock_messages):
+        response = self.client.post('/accounts/send_login_email', data={
+            'email': 'edith@example.com'
+        })
+
+        expected = "Check your email, we've sent you a link you can use to log in."
+        self.assertEqual(
+            mock_messages.success.call_args,
+            call(response.wsgi_request, expected),
+        )
